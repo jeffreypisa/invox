55495c8983964f73905b27dd81e4a6a5effb1e49
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2021-12-20 10:11:59 +0000

    [TASK] Add mobilemenu

--- lib/timber.php
+++ lib/timber.php
@@ -85,6 +85,7 @@ class StarterSite extends Timber\Site {
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
 		$context['menu']  = new Timber\Menu('menu');
+		$context['mobilemenu']  = new Timber\Menu('mobilemenu');
 		$context['footermenu']  = new Timber\Menu('footermenu');
 		
 		$context['footer_1'] = Timber::get_widgets('footer_1');
